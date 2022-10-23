8276b04fc2b3972a79459c861b4b8b1b707012c5
Hitesh Meta <meta.hitesh85@gmail.com>
2022-10-23 19:20:38 +0530

    Landing Page cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,21 @@ import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>hiteshmeta</title>
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
