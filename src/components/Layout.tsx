136c1774b5b8672d8db27d1b77898507e6d4b7f9
Hitesh Meta <meta.hitesh85@gmail.com>
2022-10-24 23:45:30 +0530

    Add Mobile View Navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,13 @@ import React, { ReactElement } from "react";
 import Footer from "./Footer";
 import Nav from "./Nav/Nav";
 import { Toaster } from "react-hot-toast";
+import MobileNav from "./Nav/MobileNav";
 
 const Layout = ({ children }: { children: ReactElement }) => {
   return (
     <>
       <Nav/>
+      <MobileNav/>
       <div className={"container relative min-h-[100vh] pb-20"}>
         {children}
         <Footer/>
