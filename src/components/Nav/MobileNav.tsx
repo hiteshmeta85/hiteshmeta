d632abd735440e395e28acb37ffaad6a0f4b1615
Hitesh Meta <meta.hitesh85@gmail.com>
2022-11-12 01:19:01 +0530

    Add Pinned Repos to Index Page

--- src/components/Nav/MobileNav.tsx
+++ src/components/Nav/MobileNav.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { NavbarItems } from "./NavItems";
 import MobileNavItem from "./MobileNavItem";
-import ThemeButton from "./ThemeButton";
+import dynamic from "next/dynamic";
+const ThemeButton = dynamic(() => import("./ThemeButton"), { ssr: false });
 
 const MobileNav = () => {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,48 @@
-import type { NextPage } from "next";
 import Layout from "../components/Layout";
 import Image from "next/future/image";
 import Avatar from "../assets/avatar.jpeg";
+import axios from "axios";
+import PinnedRepos from "../components/PinnedRepos";
+import { useEffect, useState } from "react";
 
-const Home: NextPage = () => {
+const Home = () => {
+  const [pinnedRepos, setPinnedRepos] = useState([]);
+
+  const fetchData = async () => {
+    await axios(
+      `https://gh-pinned-repos.egoist.dev/?username=hiteshmeta85`,
+    ).then(function (response) {
+      setPinnedRepos(response.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Layout>
-      <div className="flex flex-col-reverse justify-center gap-4 lg:flex-row lg:gap-16">
-        <div className="leading-none">
-          <h2 className="heading mb-2">Hitesh</h2>
-          <p className="h5 mb-4">Full Stack Web Developer</p>
-          <p className="text">
-            I build web apps! I&apos;m a Full-Stack Web Developer and a
-            Third-year Computer Science Student, pursuing my degree from Pillai
-            College of Engineering (Mumbai University), currently living in
-            Mumbai, India.
-          </p>
+      <div>
+        <div className="flex flex-col-reverse justify-center gap-4 lg:flex-row lg:gap-16">
+          <div className="leading-none">
+            <h2 className="heading mb-2">Hitesh</h2>
+            <p className="h5 mb-4">Full Stack Web Developer</p>
+            <p className="text">
+              I build web apps! I&apos;m a Full-Stack Web Developer and a
+              Third-year Computer Science Student, pursuing my degree from
+              Pillai College of Engineering (Mumbai University), currently
+              living in Mumbai, India.
+            </p>
+          </div>
+          <div className="min-w-fit">
+            <Image
+              src={Avatar}
+              alt="avatar"
+              className="h-32 w-32 rounded-full"
+            />
+          </div>
         </div>
-        <div className="min-w-fit">
-          <Image src={Avatar} alt="avatar" className="h-32 w-32 rounded-full" />
-        </div>
+        {pinnedRepos.length > 0 && <PinnedRepos pinnedRepos={pinnedRepos} />}
       </div>
     </Layout>
   );
