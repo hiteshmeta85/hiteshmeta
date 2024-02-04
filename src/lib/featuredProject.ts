77849ddba71a728a4ef2ee4ae41b33aa82c269e5
Hitesh Meta <meta.hitesh85@gmail.com>
2024-02-04 21:53:30 +0530

    Update Featured Projects

--- src/lib/featuredProject.ts
+++ src/lib/featuredProject.ts
@@ -14,6 +14,24 @@ export const featuredProjects: FeaturedProjects = [
     link: "https://csi.pce.ac.in/",
   },
   {
+    title: "Gyanbot AI",
+    description:
+      "The ultimate chatbot solution designed for commercial applications. Built using Next.js, Typescript, React Query and Shadcn.",
+    link: "https://gyanbot.ai/",
+  },
+  {
+    title: "TravHub AI",
+    description:
+      "AI Generated Travel Itineraries. Built using Next.js, Typescript, React Query and Shadcn.",
+    link: "https://travhub.ai/",
+  },
+  {
+    title: "BOG BEACON",
+    description:
+      "Generate Textbooks using AI. Built using Next.js, Typescript, TailwindCSS, Shadcn, and Vercel",
+    link: "https://the-textbook-project-web.vercel.app/",
+  },
+  {
     title: "GPod AI",
     description:
       "Convert Articles to Engaging Podcasts using AI. Built using Next.js, TailwindCSS, Typescript, Supabase, Stripe and Vercel.",
@@ -24,30 +42,5 @@ export const featuredProjects: FeaturedProjects = [
     description:
       "Web Application for Press Information Bureau (Govt. of India) that facilitates conversion of Text to Video for Press Releases.",
     link: "https://visualx-ai.vercel.app/",
-  },
-
-  {
-    title: "NDRF Relief Portal",
-    description:
-      "Web Portal for NDRF team to help identify disaster related posts and tweets, present vital information and trace geolocation.",
-    link: "https://homebrew-web.vercel.app/",
-  },
-  {
-    title: "BOG BEACON",
-    description:
-      "Generate Textbooks using AI. Built using Next.js, Typescript, TailwindCSS, Shadcn, and Vercel",
-    link: "https://the-textbook-project-web.vercel.app/",
-  },
-  {
-    title: "Neuz Org Saas Platform",
-    description:
-      "Local Publishers Focused SaaS and Consumer Application. Built using Next.js, TailwindCSS, React-Hook-Form, and Express.js",
-    link: "https://neuzorg.com/",
-  },
-  {
-    title: "Neuz Org Website",
-    description:
-      "Landing page for Neuz Org built using Next.js, TailwindCSS, and Directus CMS.",
-    link: "https://neuzorg.com/",
   },
 ];
